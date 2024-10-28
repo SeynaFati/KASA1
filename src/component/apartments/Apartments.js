a2ff820d96f0e8adf629d865d97bc695aeccfee4
Seynabou Fati <seynab.fati@hotmail.fr>
2024-10-28 23:05:13 +0100

    few one

--- src/component/apartments/Apartments.js
+++ src/component/apartments/Apartments.js
@@ -12,7 +12,7 @@ function Apartments() {
 
   useEffect(() => {
     console.log(`Fetching logement with id: ${id}`);
-    fetch(`http://kasa-project-production.up.railway.app/logements/${id}`)
+    fetch(`http://kasa1-production.up.railway.app/logements/${id}`)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
